b322a1e69e28af4c2024118fa1640ad5396dbf75
Darin Minamoto <darinm223@gmail.com>
2021-01-21 16:28:33 -0800

    Adds function to force debugger to show variable in frame's locals

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,6 +1,6 @@
 (defpackage utils
   (:use :cl :iterate)
-  (:export :xor :split-list))
+  (:export :xor :split-list :fix-debug))
 (in-package :utils)
 
 (defmacro-clause (FOR var in-lines stream)
@@ -22,3 +22,35 @@
           (progn
             (collect (subseq l start i))
             (setf start (+ i 1))))))
+
+(defun fix-debug (v)
+  "Fixes an annoyance with SBCL's debugger where certain values
+in let bindings won't show up in the debugger's list of
+local values for that frame.
+
+If you run this:
+
+```
+(defun foo ()
+  (declare (optimize (debug 3)))
+  (let ((a blah))
+    (break)
+    (print a)))
+
+(foo)
+```
+
+And `a` doesn't show up in the locals list for the `(FOO)` frame,
+then try wrapping `blah` with fix-debug:
+
+```
+(defun foo ()
+  (declare (optimize (debug 3)))
+  (let ((a (fix-debug blah)))
+    (break)
+    (print a)))
+
+(foo)
+```
+  "
+  (first (list v)))
